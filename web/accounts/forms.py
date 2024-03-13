3ec3ecc4fcf266f5b769b08021d79255b4a0835b
ellerAvery <143408407+ellerAvery@users.noreply.github.com>
2024-03-13 21:23:41 +0000

    First set of functions and test folder

--- web/accounts/forms.py
+++ web/accounts/forms.py
@@ -8,6 +8,19 @@ class LoginForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
 
+    def loginExists(email, password):
+        loginUser = User()
+
+        loginUser.email = email
+        loginUser.password = password
+
+        for(User in users):
+            if(loginUser.email == User.email and loginUser.password == User.password):
+                return True
+        
+        return False
+
+
 
 class RegisterForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired(), Email(message=None), Length(min=5, max=10)])
@@ -25,5 +38,23 @@ class RegisterForm(FlaskForm):
             return False
         return True
     
+    def passLenValid(self):
+        if(self.password.Length < 20 and self.password.Length > 8):
+            return True
+        else:
+            self.email.errors.append("Invalid password length")
+            return False
 
+    def userLenValid(self):
+        if(self.email.Length < 10 and self..Length > 5):
+            return True
+        else:
+            self.email.errors.append("Invalid email length")
+            return False
 
+    def keyLenValid(self):
+        if(self.passkey.Length < 30 and self.passkey.Length > 10):
+            return True
+        else:
+            self.email.errors.append("Invalid self.passkey length")
+            return False
