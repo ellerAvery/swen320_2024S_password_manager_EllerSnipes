a147bb0adb8fe7202161d3453347bc7dbf1ae1b2
c4snipes <cole.snipes@icloud.com>
2024-03-18 18:17:48 -0400

    Add username column and set unique usernames

---
+++ migrations/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    created_at = Column(Integer, server_default=func.now())
