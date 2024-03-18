7b10b8e451994266232a06aa46a14a3b404fe529
c4snipes <cole.snipes@icloud.com>
2024-03-18 17:01:10 -0400

    Add encryption and decryption functionality to core views

--- web/core/views.py
+++ web/core/views.py
@@ -1,10 +1,44 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+from web.accounts.models import User
 
-accounts_bp = Blueprint("accounts", __name__)
+core_bp = Blueprint("core", __name__)
 
-# Starts the Flask app
-@accounts_bp.route('/')
+@core_bp.route("/encrypt", methods=["GET", "POST"])
+@login_required
+def encrypt():
+    if request.method == "POST":
+        password_text = request.form.get('passwordTextE')
+        key_text = request.form.get('key')
+        user = User.query.filter_by(username=current_user.username).first()
+        if user: 
+            encrypted_text = user.encrypt_password(password_text, key_text)
+            return render_template("core/encrypt.html", encrypted_text=encrypted_text)
+        else: 
+            flash("User not found or not logged in.", "error")
+            return redirect(url_for("auth.login"))
+    return render_template("core/encrypt.html")
+
+@core_bp.route("/decrypt", methods=["GET", "POST"])
+@login_required
+def decrypt():
+    if request.method == "POST":
+        encrypt_text = request.form.get('encryptedTextD') 
+        user = User.query.filter_by(username=current_user.username).first()
+        if user:
+            decrypted_text = user.decrypt_password(encrypt_text)
+            return render_template("core/decrypt.html", decrypted_text=decrypted_text)
+        else:
+            flash("User not found or not logged in.", "error")
+            return redirect(url_for("auth.login"))
+    return render_template("core/decrypt.html")
+
+@core_bp.route("/")
 @login_required
 def home():
-    return render_template("accounts/login.html")
+    return render_template("core/encrypt.html")
+
+@core_bp.route("/list")
+@login_required
+def list():
+    return render_template("core/list.html")
