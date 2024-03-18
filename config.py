7040160416e431a328386bdbc3cdc4706c9eb574
c4snipes <cole.snipes@icloud.com>
2024-03-18 16:24:02 -0400

    adding TestingConfig to the Configuration fie

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = config("SECRET_KEY", default="guess-me")
     SQLALCHEMY_DATABASE_URI = DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     WTF_CSRF_ENABLED = True
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
@@ -20,3 +20,9 @@ class DevelopmentConfig(Config):
     DEBUG = True
     WTF_CSRF_ENABLED = False
     DEBUG_TB_ENABLED = True
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///testdb.sqlite"
+    WTF_CSRF_ENABLED = False
