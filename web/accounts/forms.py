03cf5b8977ca3759ea3efdd5b10d1bc2a87c10dc
c4snipes <cole.snipes@icloud.com>
2024-03-18 16:27:30 -0400

    Update function to handle edge cases

--- web/accounts/forms.py
+++ web/accounts/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length
+
+from web.accounts.models import User
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('User', validators=[DataRequired(), User()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('User', validators=[DataRequired(), User()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
     token = StringField('Token', validators=[DataRequired(), Length(min=10, max=30)])
